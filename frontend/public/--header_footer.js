96e4278e8824ee694d1cbef4c09584005e7edd0f
HelenaMayumiTomokane <helena.tomokane@gmail.com>
2025-03-26 13:39:05 -0300

    Primeiro commit

--- frontend/public/--header_footer.js
+++ frontend/public/--header_footer.js
@@ -4,13 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             document.getElementById("header_container").innerHTML = data; 
         })
-    
-    fetch("__header1.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("body_header_container").innerHTML = data; 
-        })
-
+        
     fetch("__footer.html")
         .then(response => response.text())
         .then(data => {
